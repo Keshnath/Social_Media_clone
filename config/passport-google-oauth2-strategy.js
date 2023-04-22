3a8b9629576455a5a075f675d58471a981875c48
keshnath <princeshubham.a@gmail.com>
2023-04-22 13:25:13 +0530

    env

--- config/passport-google-oauth2-strategy.js
+++ config/passport-google-oauth2-strategy.js
@@ -2,11 +2,14 @@ const passport = require('passport');
 const googleStrategy = require('passport-google-oauth').OAuth2Strategy
 const crypto = require('crypto');
 const User = require('../models/user');
+const dotenv = require('dotenv');
+
+dotenv.config({path: "./config.env"})
 
 
 passport.use(new googleStrategy({
     clientID : "47467322303-jc24r54if3mkt0dhqa3tdvfr0pu9ab2q.apps.googleusercontent.com" ,
-    clientSecret : "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",
+    clientSecret : process.env.KEY,
     callbackURL :"http://localhost:8000/users/auth/google/callback" 
 },function(accessToken , refreshToken , profile , done){
     User.findOne({email : profile.emails[0].value}).exec(function(err , user){
